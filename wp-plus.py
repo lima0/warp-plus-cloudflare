560fbd3af845ba7c9da8403878a4a348a7012a27
Selim <47898885+lima0@users.noreply.github.com>
2020-06-06 12:47:16 +0200

    rewrote print statements, removed unused variables in loop

--- wp-plus.py
+++ wp-plus.py
@@ -7,26 +7,27 @@ import time
 import os
 import sys
 import pathlib
+
 script_version = '4.0.0'
 window_title   = f"WARP-PLUS-CLOUDFLARE By ALIILAPRO (version {script_version})"
 os.system('title ' + window_title if os.name == 'nt' else 'PS1="\[\e]0;' + window_title + '\a\]"; echo $PS1')
 os.system('cls' if os.name == 'nt' else 'clear')
-print('      _______ _      __________________       _______ _______ _______ _______\n'
-'     (  ___  | \     \__   __|__   __( \     (  ___  |  ____ |  ____ |  ___  )\n'
-'     | (   ) | (        ) (     ) (  | (     | (   ) | (    )| (    )| (   ) |\n'
-'     | (___) | |        | |     | |  | |     | (___) | (____)| (____)| |   | |\n'
-'     |  ___  | |        | |     | |  | |     |  ___  |  _____)     __) |   | |\n'
-'     | (   ) | |        | |     | |  | |     | (   ) | (     | (\ (  | |   | |\n'
-'     | )   ( | (____/\__) (_____) (__| (____/\ )   ( | )     | ) \ \_| (___) |\n'
-'     |/     \(_______|_______|_______(_______//     \|/      |/   \__(_______)\n')
-print ("[+] ABOUT SCRIPT:")
-print ("[-] With this script, you can getting unlimited GB on Warp+.")
-print (f"[-] Version: {script_version}")
-print ("--------")
-print ("[+] THIS SCRIPT CODDED BY ALIILAPRO") 
-print ("[-] SITE: aliilapro.github.io") 
-print ("[-] TELEGRAM: aliilapro")
-print ("--------")
+print(f'''      _______ _      __________________       _______ _______ _______ _______
+     (  ___  | \     \__   __|__   __( \     (  ___  |  ____ |  ____ |  ___  )
+     | (   ) | (        ) (     ) (  | (     | (   ) | (    )| (    )| (   ) |
+     | (___) | |        | |     | |  | |     | (___) | (____)| (____)| |   | |
+     |  ___  | |        | |     | |  | |     |  ___  |  _____)     __) |   | |
+     | (   ) | |        | |     | |  | |     | (   ) | (     | (\ (  | |   | |
+     | )   ( | (____/\__) (_____) (__| (____/\ )   ( | )     | ) \ \_| (___) |
+     |/     \(_______|_______|_______(_______//     \|/      |/   \__(_______)
+[+] ABOUT SCRIPT:
+[-] With this script, you can getting unlimited GB on Warp+.
+[-] Version: {script_version}
+--------
+[+] THIS SCRIPT CODDED BY ALIILAPRO
+[-] SITE: aliilapro.github.io
+[-] TELEGRAM: aliilapro
+''')
 
 def newID():
 	while True:
@@ -53,7 +54,7 @@ def progressBar():
 	save_anim     = animation[progress_anim % len(animation)]
 	percent       = 0
 	while True:
-		for i in range(10):
+		for _ in range(10):
 			percent += 1
 			sys.stdout.write(f"\r[+] Waiting response...  " + save_anim + f" {percent}%")
 			sys.stdout.flush()
@@ -69,14 +70,17 @@ def genString(stringLength):
 		letters = string.ascii_letters + string.digits
 		return ''.join(random.choice(letters) for i in range(stringLength))
 	except Exception as error:
-		print(error)		    
+		print(error)
+
 def digitString(stringLength):
 	try:
 		digit = string.digits
 		return ''.join((random.choice(digit) for i in range(stringLength)))    
 	except Exception as error:
-		print(error)	
+		print(error)
+
 url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
+
 def run():
 	try:
 		install_id = genString(22)
@@ -100,8 +104,7 @@ def run():
 		status_code = response.getcode()	
 		return status_code
 	except Exception as error:
-		print("")
-		print(error)	
+		print(f"\n{error}")	
 
 if pathlib.Path("referrer.txt").exists():
 	while True:
@@ -120,28 +123,34 @@ else:
 
 g = 0
 b = 0
+
 while True:
 	os.system('cls' if os.name == 'nt' else 'clear')
-	print("")
-	print("                  WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO")
-	print("")
+	print('''
+	                  WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO)
+
+		''')
 	sys.stdout.write("\r[+] Sending request...   [□□□□□□□□□□] 0%")
 	sys.stdout.flush()
 	result = run()
 	if result == 200:
 		g += 1
 		progressBar()
-		print(f"\n[-] WORK ON ID: {referrer}")    
-		print(f"[:)] {g} GB has been successfully added to your account.")
-		print(f"[#] Total: {g} Good {b} Bad")
+		print(f'''
+		[-] WORK ON ID: {referrer}
+		[:)] {g} GB has been successfully added to your account.
+		[#] Total: {g} Good {b} Bad
+		'''
+		)
 		for i in range(18,0,-1):
 			sys.stdout.write(f"\r[*] After {i} seconds, a new request will be sent.")
 			sys.stdout.flush()
 			time.sleep(1)
 	else:
 		b += 1
-		print("[:(] Error when connecting to server.")
-		print(f"[#] Total: {g} Good {b} Bad")
+		print(f'''[:(] Error when connecting to server.
+		[#] Total: {g} Good {b} Bad
+		''')
 		for i in range(10,0,-1):
 			sys.stdout.write(f"\r[*] Retrying in {i}s...")
 			sys.stdout.flush()
